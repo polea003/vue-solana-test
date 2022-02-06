c094a9877ebe89d661b53709c4e68dd9bc58ca7d
polea003 <polea003@fiu.edu>
2022-02-06 09:45:43 -0500

    changed origional test script

--- AnchorValentine/tests/AnchorValentine.js
+++ AnchorValentine/tests/AnchorValentine.js
@@ -1,14 +1,39 @@
+// const anchor = require('@project-serum/anchor');
+
+// describe('AnchorValentine', () => {
+
+//   // Configure the client to use the local cluster.
+//   anchor.setProvider(anchor.Provider.env());
+
+//   it('Is initialized!', async () => {
+//     // Add your test here.
+//     const program = anchor.workspace.AnchorValentine;
+//     const tx = await program.rpc.initialize();
+//     console.log("Your transaction signature", tx);
+//   });
+// });
+
+
 const anchor = require('@project-serum/anchor');
 
-describe('AnchorValentine', () => {
+const main = async() => {
+  console.log("🚀 Starting test...")
 
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
+  const program = anchor.workspace.AnchorValentine;
+  const tx = await program.rpc.initialize();
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.AnchorValentine;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
-  });
-});
+  console.log("📝 Your transaction signature", tx);
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
